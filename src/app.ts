23ea40bf7b3b0b0fef1947cb9902a93edbddcc14
ChelesteWang <1581446178@qq.com>
2022-03-01 23:36:57 +0800

    chore: use FC

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,7 @@
-import { Component } from 'react'
-import './app.scss'
+import "./app.scss";
 
-class App extends Component {
-
-  componentDidMount () {}
-
-  componentDidShow () {}
-
-  componentDidHide () {}
-
-  componentDidCatchError () {}
-
-  // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
-  }
+function App(props) {
+  return props.children;
 }
 
-export default App
+export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,11 @@
-import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
 
-export default class Index extends Component {
 
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
-    )
-  }
+export default function Index() {
+  return (
+    <View className='index'>
+      <Text>Hello world!</Text>
+    </View>
+  )
 }
