cc15ac64ff53afcfc34710d74c024de7c28e8b00
ChelesteWang <1581446178@qq.com>
2022-03-01 23:53:28 +0800

    feat: 尝试布局

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from '@tarojs/components'
-import './index.scss'
-
+import { View, Text } from "@tarojs/components";
+import "./index.scss";
 
 export default function Index() {
   return (
-    <View className='index'>
+    <View
+      className='index'
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Text>Hello world!</Text>
     </View>
-  )
+  );
 }
